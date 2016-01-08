3e51d0b291497fe33946885ed7524717f07d454c
Rand01ph <tanyawei1991@gmail.com>
2016-01-08 15:44:31 +0800

    新增永久素材相关接口

--- wechat_sdk/basic.py
+++ wechat_sdk/basic.py
@@ -53,6 +53,9 @@ class WechatBasic(object):
         self.__access_token_expires_at = access_token_expires_at
         self.__jsapi_ticket = jsapi_ticket
         self.__jsapi_ticket_expires_at = jsapi_ticket_expires_at
+
+        self.__checkssl = checkssl
+
         self.__is_parse = False
         self.__message = None
 
@@ -843,6 +846,142 @@ class WechatBasic(object):
             }
         )
 
+    def add_material_news(self, news):
+        """
+        新增永久素材
+        详情请参考
+        http://mp.weixin.qq.com/wiki/14/7e6c03263063f4813141c3e17dd4350a.html
+        :param news: 图文素材数组
+        :return: 返回的 JSON 数据包
+        """
+        self._check_appid_appsecret()
+
+        news_data = []
+        for new in news:
+            news_data.append({
+                'title': new['title'],
+                'thumb_media_id': new['thumb_media_id'],
+                'author': new.get('author', ''),
+                'digest': new.get('digest', ''),
+                'show_cover_pic': new.get('show_cover_pic', 0),
+                'content': new['content'],
+                'content_source_url': new.get('content_source_url', '')
+            })
+        return self._post(
+            url='https://api.weixin.qq.com/cgi-bin/material/add_news',
+            data={
+                'articles': news_data
+            }
+        )
+
+    def update_material_news(self, media_id, index, news):
+        """
+        修改永久素材
+        详情请参考
+        http://mp.weixin.qq.com/wiki/14/7e6c03263063f4813141c3e17dd4350a.html
+        :param media_id: 要修改的图文消息的 id
+        :param index: 要更新的文章在图文消息中的位置（多图文消息时，此字段才有意义），第一篇为 0
+        :param articles: 图文素材数组
+        :return: 返回的 JSON 数据包
+        """
+        self._check_appid_appsecret()
+
+        news_data = []
+        for new in news:
+            news_data.append({
+                'title': new['title'],
+                'thumb_media_id': new['thumb_media_id'],
+                'author': new.get('author', ''),
+                'digest': new.get('digest', ''),
+                'show_cover_pic': new.get('show_cover_pic', 0),
+                'content': new['content'],
+                'content_source_url': new.get('content_source_url', '')
+            })
+        return self._post(
+            url='https://api.weixin.qq.com/cgi-bin/material/update_news',
+            data={
+                'media_id': media_id,
+                'index': index,
+                'articles': news_data
+            }
+        )
+
+    def get_material(self, media_id):
+        """
+        获取永久素材
+        详情请参考
+        http://mp.weixin.qq.com/wiki/4/b3546879f07623cb30df9ca0e420a5d0.html
+        :param media_id: 素材的media_id
+        :return: 图文素材返回图文列表，其它类型为素材的内容
+        """
+        self._check_appid_appsecret()
+
+        def _processor(res):
+            if isinstance(res, dict):
+                # 图文素材
+                return res.get('news_item', [])
+            return res
+
+        res = self._post(
+            url= 'https://api.weixin.qq.com/cgi-bin/material/get_material',
+            data={
+                'media_id': media_id
+            },
+            result_processor=_processor
+        )
+        return res
+
+    def del_material(self, media_id):
+        """
+        删除永久素材
+        详情请参考
+        http://mp.weixin.qq.com/wiki/5/e66f61c303db51a6c0f90f46b15af5f5.html
+        :param media_id: 素材的media_id
+        :return: 返回的 JSON 数据包
+        """
+        self._check_appid_appsecret()
+
+        return self._post(
+            url= 'https://api.weixin.qq.com/cgi-bin/material/del_material',
+            data={
+                'media_id': media_id
+            }
+        )
+
+    def get_material_count(self):
+        """
+        获取素材总数
+        详情请参考
+        http://mp.weixin.qq.com/wiki/16/8cc64f8c189674b421bee3ed403993b8.html
+        :return: 返回的 JSON 数据包
+        """
+        self._check_appid_appsecret()
+
+        return self._get(
+            url='https://api.weixin.qq.com/cgi-bin/material/get_materialcount'
+        )
+
+    def batchget_material(self, media_type, offset=0, count=20):
+        """
+        批量获取永久素材列表
+        详情请参考
+        http://mp.weixin.qq.com/wiki/15/8386c11b7bc4cdd1499c572bfe2e95b3.html
+        :param media_type: 媒体文件类型，分别有图片（image）、语音（voice）、视频（video）和缩略图（news）
+        :param offset: 从全部素材的该偏移位置开始返回，0 表示从第一个素材返回
+        :param count: 返回素材的数量，取值在1到20之间
+        :return: 返回的 JSON 数据包
+        """
+        self._check_appid_appsecret()
+
+        return self._post(
+            url='https://api.weixin.qq.com/cgi-bin/material/batchget_material',
+            data={
+                'type': media_type,
+                'offset': offset,
+                'count': count
+            }
+        )
+
     @property
     def access_token(self):
         self._check_appid_appsecret()
@@ -918,6 +1057,7 @@ class WechatBasic(object):
         r = requests.request(
             method=method,
             url=url,
+            verify=self.__checkssl,
             **kwargs
         )
         r.raise_for_status()
